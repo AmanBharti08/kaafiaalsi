3b9aa8f32bd81a4a614e5e7c3780b46e14e37d71
Aman <amanbharti0897@gmail.com>
2026-05-22 15:06:46 +0530

    Navbar Styling changed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="flex justify-center w-[30%] p-2 px-5 fixed bg-white/40 z-10 rounded-4xl mt-5 text-2xl shadow-xl">
+    <div className="flex justify-center w-[30%] p-2 px-10 fixed bg-white/40 z-10 rounded-4xl mt-5 lg:text-2xl md:text-xl sm:text-lg shadow-xl">
       <div className="flex space-x-18">
         <a href="#" className="text-blue-700 hover:text-amber-100">
           Home
